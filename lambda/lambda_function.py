c5d88cd5068beda90d06e67c0c03291e6527f96e
Ashim <akkiyemba01@gmail.com>
2024-10-12 14:53:37 +1100

    testing configuration

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps("hello updated lambda from vscode")
+        'body': json.dumps("hello, checking if github workflow configuration is working")
     }
